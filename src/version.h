3bc8cc2bd274aba5cbeedd15e515dd4c4139d8c0
Robert James Clay <jame@users.sourceforge.net>
2010-02-05 23:19:26 +0000

    Bump version to v3.2.9a

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
-/* $Id: version.h,v 1.2 2010/01/20 22:32:18 jame Exp $  */
+/* $Id: version.h,v 1.3 2010/02/05 23:19:26 jame Exp $  */
 /* Version File */
 
-#define MAKENL_VERSION     "3.2.9"
+#define MAKENL_VERSION     "3.2.9a"
 
 #define MAKENL_DEDICATION  "Dedicated to Ben Baker - The Original Author of MakeNL\n\n"
