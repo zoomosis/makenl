922eb3ec74860b197eddf94458d8655686319164
Robert James Clay <jame@users.sourceforge.net>
2013-02-07 14:56:40 +0000

    Release MakeNL v3.3.5

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
-/* $Id: version.h,v 1.20 2013/02/07 14:18:57 jame Exp $  */
+/* $Id: version.h,v 1.21 2013/02/07 14:56:40 jame Exp $  */
 
 #include "os.h"
 
-#define MAKENL_VERSION      "3.3.4"
+#define MAKENL_VERSION      "3.3.5"
 
 #define MAKENL_LONG_VERSION "MakeNL " MAKENL_VERSION " (" MAKENL_OS ") compiled with " MAKENL_CC " on " __DATE__ " " __TIME__
 
