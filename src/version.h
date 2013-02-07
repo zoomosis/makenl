faf361e34ab5e97f825c61c8b5ceeb78c39a2f29
Robert James Clay <jame@users.sourceforge.net>
2013-02-07 14:18:57 +0000

    Update docs/history.txt and src/version.h to UTF-8.

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
-/* $Id: version.h,v 1.19 2013/01/30 21:58:10 ajleary Exp $  */
+/* $Id: version.h,v 1.20 2013/02/07 14:18:57 jame Exp $  */
 
 #include "os.h"
 
@@ -8,6 +8,6 @@
 
 #define MAKENL_CREDITS \
   "This version of MakeNL is based upon work by Michael Karcher with\n" \
-  "modifications by Bjorn Felten, Robert Couture, Andrew Clarke,\n" \
+  "modifications by Björn Felten, Robert Couture, Andrew Clarke,\n" \
   "Michiel Broek, Malcolm Miles, Johan Billing & others.\n\n" \
   "Dedicated to Ben Baker - The author of the original MakeNL.\n\n"
