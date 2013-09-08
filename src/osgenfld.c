28e780d8f439980bbc868d235487b8af416435a4
Andrew Clarke <ozzmosis@users.sourceforge.net>
2013-09-08 19:34:50 +0000

    Show return code from stat().

--- src/osgenfld.c
+++ src/osgenfld.c
@@ -1,32 +1,41 @@
-/* $Id: osgenfld.c,v 1.5 2013/08/23 14:51:07 ozzmosis Exp $ */
+/* $Id: osgenfld.c,v 1.6 2013/09/08 19:34:50 ozzmosis Exp $ */
 
 #include <sys/types.h>
 #include <sys/stat.h>
 
 /* make an absolute path from given relative path */
 /* src is supposed to specify an existing directory name    */
-#define FUNCNAME "[generic] os_fulldir"
+
 int os_fulldir(char *dst, const char *src, size_t bufsiz)
 {
     char tmp[MYMAXPATH];
     struct stat st;
+    int rc;
 
     mklog(LOG_DEBUG, __FILE__ ": os_fulldir(): called with src='%s'", src);
+
     strcpy(tmp, src);
     os_remove_slash(tmp);
     mklog(LOG_DEBUG, __FILE__ ": os_fulldir(): after removing slash: '%s'", tmp);
+
     if (os_fullpath(dst, tmp, bufsiz) != 0)
     {
         mklog(LOG_DEBUG, __FILE__ ": os_fulldir(): os_fullpath failed!");
         return -1;
     }
+
     mklog(LOG_DEBUG, __FILE__ ": os_fulldir(): absolute path '%s'", dst);
-    if (stat(dst, &st) != 0)
+
+    rc = stat(dst, &st);
+
+    if (rc != 0)
     {
-        mklog(LOG_DEBUG, __FILE__ ": os_fulldir(): stat failed!");
+        mklog(LOG_DEBUG, __FILE__ ": os_fulldir(): stat('%s') rc=%d!", dst, rc);
         return -1;
     }
+
     mklog(LOG_DEBUG, __FILE__ ": os_fulldir(): st_mode is now %o", st.st_mode);
+
 #ifdef __TURBOC__
     return S_ISDIR(st.st_mode) ? 0 : -1;
 #elif defined(__IBMC__)
