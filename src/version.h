bdb06f8e06c7fc2ba0b24cc67a0aacd5f8087b8c
Andrew Clarke <ozzmosis@users.sourceforge.net>
2013-09-11 21:40:16 +0000

    Add Andrew Leary to credits.

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
-/* $Id: version.h,v 1.24 2013/09/05 01:40:50 ajleary Exp $  */
+/* $Id: version.h,v 1.25 2013/09/11 21:40:16 ozzmosis Exp $  */
 
 #include "os.h"
 
@@ -7,7 +7,10 @@
 #define MAKENL_LONG_VERSION "MakeNL " MAKENL_VERSION " (" MAKENL_OS ") compiled with " MAKENL_CC " on " __DATE__ " " __TIME__
 
 #define MAKENL_CREDITS \
-  "This version of MakeNL is based upon work by Michael Karcher with\n" \
-  "modifications by Björn Felten, Robert Couture, Andrew Clarke,\n" \
-  "Michiel Broek, Malcolm Miles, Johan Billing & others.\n\n" \
-  "Dedicated to Ben Baker - The author of the original MakeNL.\n\n"
+  "\n" \
+  "This version of MakeNL is based upon work by Michael Karcher with" "\n" \
+  "modifications by Björn Felten, Robert Couture, Andrew Clarke," "\n" \
+  "Michiel Broek, Malcolm Miles, Johan Billing, Andrew Leary & others." "\n" \
+  "\n" \
+  "Dedicated to Ben Baker - The author of the original MakeNL.\n" \
+  "\n"
